60cba68397275fc383936c0984e8a1d40d6d85a3
GoSmartSend <GoSmartSend@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-07 08:29:14 +0000

    Version 2.1.0
     - Better quote caching system integrated
     - Errors and notices handled better on checkout
     - Suburb / postcode UI improved on cart shipping estimate
     - Tail-lift delivery must be enforce for items over 30kg for non-business deliveries
     - Added second origin street address
     - Warning on character restrictions on settings screen
    
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-smart-send-australian-shipping/tags/2.1.0@1129340 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woocommerce-smartsend.php
@@ -0,0 +1,42 @@
+<?php
+/*
+	Plugin Name: Smart Send Shipping for WooCommerce
+	Plugin URI: http://digital.smartsend.com.au/plugins/woocommerce
+	Description: Add Smart Send Australian shipping calculations to Woo Commerce.
+	Version: 2.1.0
+	Author:  Smart Send
+	Author:  Paul Appleyard
+	Author URI: http://digital.smartsend.com.au/plugins/woocommerce-shipping-plugin
+	License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+/**
+ * Check if WooCommerce is active
+ **/
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) )
+{
+
+	// Paths
+
+	define( 'SSBASE', dirname( __FILE__ ) . '/' );
+	define( 'SSCACHE', SSBASE . 'cache/' );
+	define( 'SSINC', SSBASE . 'includes/' );
+	define( 'SSASSETS', SSBASE . 'assets/' );
+	define( 'SSLOGS', SSBASE . 'logs/' );
+	define( 'SSLABELS', SSBASE . 'labels/' );
+
+// SDK classes
+	foreach (glob(dirname(__FILE__)."/sdk/*.php") as $filename)
+	{
+		include $filename;
+	}
+
+	require_once('smartsend-plugin.php');
+
+// Plugin includes
+	foreach (glob(dirname(__FILE__)."/includes/*.php") as $filename)
+	{
+		include $filename;
+	}
+
+}
